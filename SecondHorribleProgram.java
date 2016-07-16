bb11dc6206fc7a083fca8580ce26a722b1bf27e4
JustinVita <skeeisjus@gmail.com>
2016-07-15 23:40:56 -0400

    Code

---
+++ SecondHorribleProgram.java
@@ -0,0 +1,9 @@
+public class SecondHorribleProgram
+{
+   public static void main(String[]args)
+   {
+      int x = 5;
+      double y = 4.5;
+      System.out.println("Our integer is "+ x +" and our real is "+ y);
+   }
+}
